0346915779ebab3957e8f22d24e5c6aebc943873
alexpeev9 <alexpeev9@abv.bg>
2022-03-18 09:11:04 +0200

    Add Design

--- assets/js/modal-popup.js
+++ assets/js/modal-popup.js
@@ -1,7 +1,23 @@
 gitbook.events.bind("page.change", function() {
-    setTimeout(loadModal(), 5000);
+    setTimeout(loadModal(), 10000);
 });
 
 function loadModal() {
     console.log("Show Message.");
+    let doc = $('section.markdown-section');
+    let modalContent = $(`
+                            <span id="modal" class="bodycontent">
+                                <p class="modal-content">
+                                    <a>Some text in the Modal..</a>
+                                </p>
+                            </span>
+                        `)
+    doc.prepend(modalContent);
+
+    window.onclick = function(event) {
+        let modalContent = document.getElementById("modal");
+        if (event.target == modalContent) {
+            modalContent.style.display = "none";
+        }
+    }
 }
