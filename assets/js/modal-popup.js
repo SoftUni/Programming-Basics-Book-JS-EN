68f6001b6ce332da957525278dfdee4f6e4b4ddc
alexpeev9 <alexpeev9@abv.bg>
2022-03-18 09:52:15 +0200

    Update Modal Idea

--- assets/js/modal-popup.js
+++ assets/js/modal-popup.js
@@ -1,22 +1,24 @@
 gitbook.events.bind("page.change", function() {
-    setTimeout(() => {
-        console.log("Show Message.");
-        let doc = $('section.markdown-section');
-        let modalContent = $(`
-                                <span id="modal" class="bodycontent">
-                                    <p class="modal-content">
-                                        <a>Some text in the Modal..</a>
-                                    </p>
-                                </span>
-                            `);
-        doc.prepend(modalContent);
-        modalContent.style.display = "block";
+    let doc = $('section.markdown-section');
+    let modalContent = $(`
+    <span id="modal" class="bodycontent">
+    <p class="modal-content">
+    <a>Some text in the Modal..</a>
+    </p>
+    </span>
+    `);
+    doc.prepend(modalContent);
 
-        window.onclick = function(event) {
-            let modalContent = document.getElementById("modal");
-            if (event.target == modalContent) {
-                modalContent.style.display = "none";
-            }
-        }
+    setTimeout(() => {
+        let modalContentBody = document.getElementById("modal");
+        modalContentBody.style.display = "block";
     }, 10000);
+
+    window.onclick = function(event) {
+        let modalContent = document.getElementById("modal");
+        if (event.target == modalContent) {
+            modalContent.style.display = "none";
+        }
+    }
+
 });
