a886e6d011289670a5b29f44ddb926243f223fe9
Svetlin Nakov <nakov@users.noreply.github.com>
2022-07-05 15:40:49 +0300

    Update top-menu.js

--- assets/js/top-menu.js
+++ assets/js/top-menu.js
@@ -3,11 +3,10 @@ gitbook.events.bind("page.change", function() {
     let topMenu = $(`
         <span class="topnav">
             <a class="logobox" target="_blank" href="https://softuni.org"><img class="logo" src="/assets/softuni-global-logo.png" alt="logo"></a>
-            <a target="_blank" href="https://softuni.org">Learn Programming</a>
+            <a target="_blank" href="https://softuni.org/learn">Learn Programming</a>
             <a target="_blank" href="https://www.youtube.com/c/CodeWithNakov">YouTube Channel</a>
             <a target="_blank" href="https://introprogramming.info">Nakov's Books</a>
         </span>
     `);
     doc.prepend(topMenu);
-
 });
