c5a136613e22865be3281eea5cfc29a087e003e3
Nikol Ruseva <nikol.n.ruseva@gmail.com>
2022-11-23 14:14:50 +0200

    Update top-menu.js

--- assets/js/top-menu.js
+++ assets/js/top-menu.js
@@ -2,8 +2,8 @@ gitbook.events.bind("page.change", function() {
     let doc = $('section.markdown-section');
     let topMenu = $(`
         <span class="topnav">
-            <a class="logobox" target="_blank" href="https://softuni.org/learn?utm_source=FreeProgrammingBooks&utm_medium=logo&utm_campaign=JavaScriptBook"><img class="logo" src="/assets/softuni-global-logo.svg" alt="logo"></a>
-            <a target="_blank" href="https://learn.softuni.org?utm_source=FreeProgrammingBooks&utm_medium=header-menu&utm_campaign=JavaScriptBook">Learn Programming</a>
+            <a class="logobox" target="_blank" href="https://softuni.org/learn/?utm_source=JavaScript&utm_medium=logo&utm_campaign=free+programming+books"><img class="logo" src="/assets/softuni-global-logo.svg" alt="logo"></a>
+            <a target="_blank" href="https://learn.softuni.org/catalog?utm_source=JavaScript&utm_medium=top+menu&utm_campaign=free+programming+books">Learn Programming</a>
             <a target="_blank" href="https://www.youtube.com/c/CodeWithNakov">YouTube Channel</a>
             <a target="_blank" href="https://introprogramming.info">Nakov's Books</a>
         </span>
